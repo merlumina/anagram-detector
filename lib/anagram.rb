63fc3f02d0372f995dbe0098dfb6d9176e9d0309
Liz <lizriderwilson@gmail.com>
2020-12-27 13:35:58 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
 # Your code goes here!
+
+require "pry"
+
+class Anagram
+
+    def initialize(word)
+        @word = word
+    end
+
+    attr_accessor :word
+
+    def match(array)
+        anagram_detected = []
+        array.each do |item|
+            if item.split("").sort == @word.split("").sort
+                anagram_detected << item
+            end
+        end
+        anagram_detected
+    end
+    
+end
